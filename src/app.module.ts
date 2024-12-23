24d2710edd159033b28dffbb82e97a86e01701e1
Marcin Mierzwa <marcin.mierzwa81@gmail.com>
2024-12-23 22:52:00 +0100

    init app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+// import config from './config/config';
+
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+    cache: true,
+    // load: [config],
+  })
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors({
+    origin: process.env.URI,
+    credentials: true
+  })
+  await app.listen(process.env.PORT);
+  console.log(`My server is running on port ${process.env.PORT}`);
 }
 bootstrap();
