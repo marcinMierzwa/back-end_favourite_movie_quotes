800b8603cb5c9b64ea38f9d4bb0ef9d7cc82fbb6
Marcin Mierzwa <marcin.mierzwa81@gmail.com>
2024-12-26 11:17:31 +0100

    update

--- src/DOMAIN/USER/user.service.ts
+++ src/DOMAIN/USER/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { User } from 'src/SCHEMA/user.schema';
+import { User } from 'src/DOMAIN/USER/SCHEMA/user.schema';
 
 @Injectable()
 export class UserService {
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { AuthModule } from './MODULES/AUTH/auth.module';
-import { UserModule } from './MODULES/USER/user.module';
+import { UserModule } from './DOMAIN/USER/user.module';
 import { MongooseModule } from '@nestjs/mongoose';
 // import config from './config/config';
 
@@ -15,7 +14,7 @@ import { MongooseModule } from '@nestjs/mongoose';
     // load: [config],
   }), 
   MongooseModule.forRoot('mongodb+srv://marcinmierzwa81:9dnhY8LHu2pCzcDB@quotes.qdnlb.mongodb.net/?retryWrites=true&w=majority&appName=Quotes'), 
-  AuthModule, UserModule,
+  , UserModule,
   ],
   controllers: [],
   providers: [],
