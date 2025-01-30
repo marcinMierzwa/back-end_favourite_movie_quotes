f782c70ebb2f245b6cfb3d36d9fd9127d7597108
Marcin Mierzwa <marcin.mierzwa81@gmail.com>
2025-01-30 20:58:35 +0100

    update

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.DATASOURCE_FRONTEND_URL,
     optionsSuccessStatus: 200,
     credentials: true
   })
-  await app.listen(process.env.DATASOURCE_PORT);
+  await app.listen(process.env.PORT ?? 3000);
   console.log(`My server is running on port ${process.env.DATASOURCE_PORT}`);
 }
 bootstrap();
